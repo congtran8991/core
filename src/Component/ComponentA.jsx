82c28707b4f291cf98649d4a772254f6b57c0345
congtran8991 <cong.tran@deliveree.com>
2023-03-22 16:00:55 +0700

    cmt abc

--- src/Component/ComponentA.jsx
+++ src/Component/ComponentA.jsx
@@ -16,14 +16,14 @@ const ComponentA = () => {
   console.log("dataFetchApi", dataFetchApi);
 
   useEffect(() => {
-    const abcde1 = async () => {
+    const abcde1222 = async () => {
       const dataParam = {
         overrideDomain: "https://jsonplaceholder.typicode.com/users",
       };
       const dataApi = await AXIOS(dataParam);
       console.log("dataApi:", dataApi);
     };
-    abcde1();
+    abcde1222();
   }, []);
 
   return (
