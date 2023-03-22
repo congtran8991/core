f4164a781b0798917290cee1292a88ee8816bbdf
congtran8991 <cong.tran@deliveree.com>
2023-03-22 16:11:27 +0700

    commit moi hon  nhanh master

--- src/Component/ComponentA.jsx
+++ src/Component/ComponentA.jsx
@@ -16,14 +16,14 @@ const ComponentA = () => {
   console.log("dataFetchApi", dataFetchApi);
 
   useEffect(() => {
-    const abcde1222333 = async () => {
+    const abcde1222333444 = async () => {
       const dataParam = {
         overrideDomain: "https://jsonplaceholder.typicode.com/users",
       };
       const dataApi = await AXIOS(dataParam);
       console.log("dataApi:", dataApi);
     };
-    abcde1222333();
+    abcde1222333444();
   }, []);
 
   return (
